c960a06d8d37c4e6dbf63c3e6e8b7c013516df11
JLCucumber <jasonli1207@foxmail.com>
2025-02-24 00:56:50 +0000

    Improve Waypoint Follower && Rearrange launch files

--- src/cw1_team_2/launch/environment_setup.launch.py
+++ src/cw1_team_2/launch/environment_setup.launch.py
@@ -1,3 +1,16 @@
+# from launch import LaunchDescription
+# from launch_ros.actions import Node
+
+# # def generate_launch_description():
+# #     return LaunchDescription([
+# #         Node(
+# #             package='cw1_team_2',
+# #             executable='obstacle_follower',
+# #             name='obstacle_follower',
+# #             output='screen'
+# #         )
+# #     ])
+
 
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, GroupAction, TimerAction
@@ -18,32 +31,96 @@ def generate_launch_description():
     # Get the use_sim_time configuration to pass to nodes and included launch files
     use_sim_time = LaunchConfiguration("use_sim_time")
 
-    # Include environment_setup.launch.py
-    environment_setup_launch = IncludeLaunchDescription(
+    # ros2 launch go2_config gazebo_mid360.launch.py
+    gazebo_launch = IncludeLaunchDescription(
+        # PythonLaunchDescriptionSource("gazebo_mid360_cw1.launch.py"),
+
         PythonLaunchDescriptionSource(
             os.path.join(
-                get_package_share_directory('cw1_team_2'),
+                get_package_share_directory("cw1_team_2"),
                 'launch',
-                'environment_setup.launch.py'
+                'gazebo_mid360_cw1.launch.py'
             )
         ),
-        launch_arguments={'use_sim_time': use_sim_time}.items()
+        launch_arguments={'use_sim_time': use_sim_time,
+                        # 'world_init_x': '-1.0',    # 你想要的新默认值
+                        # 'world_init_y': '-3.0',
+                        # 'world_init_z': '0.5',
+                        # 'world_init_heading': '1.57'
+        }.items()  # Pass sim_time
     )
 
-    # Node for cw1_edge_follower
-    edge_follower_node = Node(
+    # ros2 launch fast_lio mapping.launch.py
+    mapping_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory("fast_lio"),
+            'launch',
+            'mapping.launch.py'
+            )
+        ),
+        launch_arguments={'config_file': 'unitree_go2_mid360.yaml', 'use_sim_time': use_sim_time}.items()
+    )
+
+    # ros2 run waypoint_follower waypoint_follower
+    waypoint_follower_node = Node(
         package='cw1_team_2',
-        executable='cw1_edge_follower',
+        executable='cw1_waypoint_follower',
+        output='screen',
+        parameters=[{"use_sim_time": use_sim_time}]
+    )
+    
+    # ros2 run local_map_creator local_map_creator
+    local_map_node = Node(
+        package='local_map_creator',
+        executable='local_map_creator',
         output='screen',
         parameters=[{"use_sim_time": use_sim_time}]
     )
 
-    return LaunchDescription([
-        declare_use_sim_time,
-        environment_setup_launch,
-        edge_follower_node,
+    # ros2 run static_tf_node to connect the robot with the camera
+    static_tf_node = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        arguments=['0', '0', '0', '1.57', '0', '0', 'odom', 'body'],
+        output='screen',
+        parameters=[{"use_sim_time": use_sim_time}]
+    )
+
+    # segment groups
+    gazebo_group = GroupAction([
+        gazebo_launch
+    ])
+    mapping_group = GroupAction([
+        mapping_launch
+    ])
+    local_map_group = GroupAction([
+        local_map_node
+    ])
+    static_tf_group = GroupAction([
+        static_tf_node
+    ])
+    waypoint_follower_group = GroupAction([
+        waypoint_follower_node
     ])
 
+    return LaunchDescription([
+        declare_use_sim_time,  # Declare use_sim_time
+        gazebo_group,
+        mapping_group,
+        local_map_group,
+        static_tf_group,
+        waypoint_follower_group,
+    ])
+
+
+
+# from launch import LaunchDescription
+# from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, GroupAction, TimerAction
+# from launch.substitutions import LaunchConfiguration
+# from launch.launch_description_sources import PythonLaunchDescriptionSource
+# from ament_index_python.packages import get_package_share_directory
+# from launch_ros.actions import Node
+# import os
 
 # def generate_launch_description():
 #     # Declare the use_sim_time argument
@@ -60,17 +137,11 @@ def generate_launch_description():
 #     gazebo_launch = IncludeLaunchDescription(
 #         PythonLaunchDescriptionSource(
 #             os.path.join(
-#                 "/workspace/comp0244-go2/scripts",
-#                 "gazebo_mid360_cw1.launch.py"
+#                 get_package_share_directory("go2_config"),
+#                 'launch',
+#                 'gazebo_mid360.launch.py'
 #             )
 #         ),
-#         # PythonLaunchDescriptionSource(
-#         #     os.path.join(
-#         #         get_package_share_directory("go2_config"),
-#         #         'launch',
-#         #         'gazebo_mid360.launch.py'
-#         #     )
-#         # ),
 #         launch_arguments={'use_sim_time': use_sim_time,
 #                         # 'world_init_x': '-1.0',    # 你想要的新默认值
 #                         # 'world_init_y': '-3.0',
@@ -138,5 +209,7 @@ def generate_launch_description():
 #         mapping_group,
 #         local_map_group,
 #         static_tf_group,
-#         waypoint_follower_group,
+#         waypoint_follower_group
 #     ])
+
+
