6c0c1ef65c5f563351a27a9cbad6f6220822aa2b
EthanJia <804537158@qq.com>
2025-03-21 18:16:41 +0800

    Update: Integrate CW1 Waypoint Follower into Goalpose Follower

--- src/cw2_team_2/setup.py
+++ src/cw2_team_2/setup.py
@@ -21,6 +21,7 @@ setup(
     entry_points={
         'console_scripts': [
             'foot_contact_estimator = cw2_team_2.task1.foot_contact_estimator:main',
+            'cw2_goalpose_follower = cw2_team_2.task2.cw2_goalpose_follower:main',
         ],
     },
 )
